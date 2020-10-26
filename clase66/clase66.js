0074a7aec41d94f115fb90e2224ff7e7bfa21cd8
Sofia Ferrer <chopotolina@gamil.com>
2020-10-26 19:23:54 -0300

    continue con consignas

--- clase66/clase66.js
+++ clase66/clase66.js
@@ -103,8 +103,60 @@ console.log(esConsonante('i')); // false
 console.log(esConsonante('e')); // false
 console.log(esConsonante('r')); // true*/
 
+/* 6) Crear una función esHoraValida que tome como argumento un string hora con el formato HH:mm y 
+determine si es una hora válida del día o no*/
 
-/* 5) Crear una función esParOImpar que acepte como argumento un numero y devuelva el string 
+const esHoraValida = (str) => {
+    const hora = str.split(":")
+    return (hora[0] <= 23 && hora[1] <= 59)
+}
+
+/*console.log (esHoraValida('12:23')) // true
+console.log (esHoraValida('12:65')) // false
+console.log (esHoraValida('28:05')) // false
+console.log (esHoraValida('00:00')) // true*/
+
+
+/* 7) Crear una función puedeRenovarCarnet que tome como argumentos tres booleanos, pasoTests, 
+tieneMultasImpagas y pagoImpuestos, y devuelva true si una persona está habilitada para renovar
+su carnet de conducir o false si no. Una persona puede renovar su carnet si pasó los tests, 
+no tiene multas impagas y pagó todos los impuestos*/
+
+const pasoTests = true
+const tieneMultasImpagas = true
+const pagoImpuestos = true
+
+
+  
+/*puedeRenovarCarnet(true, true, true)  // false
+puedeRenovarCarnet(true, true, false)   // false
+puedeRenovarCarnet(true, false, true)   // true
+puedeRenovarCarnet(true, false, false)  // false
+puedeRenovarCarnet(false, true, true)   // false
+puedeRenovarCarnet(false, true, false)  // false
+puedeRenovarCarnet(false, false, true)  // false
+puedeRenovarCarnet(false, false, false) // false*/
+
+
+/* 8) Crear una función puedeGraduarse que tome como argumentos dos números asistencia y 
+materiasAprobadas y un booleano tesisAprobada, y devuelva true si una persona puede gruadarse 
+o false si no. Una persona puede graduarse si tiene un 75% de asistencia o más, 50 materias 
+aprobadas o más y la tesis aprobada.*/
+
+const puedeGraduarse = (asistencia, materias, tesis) => {
+    return (asistencia > 75 && materias >= 50 && tesis)
+}
+
+/*console.log(puedeGraduarse(80, 50, true))  // true
+console.log(puedeGraduarse(80, 50, false)) // false
+console.log(puedeGraduarse(80, 45, true))  // false
+console.log(puedeGraduarse(80, 45, false)) // false
+console.log(puedeGraduarse(65, 50, true))  // false
+console.log(puedeGraduarse(33, 55, false)) // false
+console.log(puedeGraduarse(42, 45, true))  // false
+console.log(puedeGraduarse(28, 45, false)) // false*/
+
+/* 9) Crear una función esParOImpar que acepte como argumento un numero y devuelva el string 
 par si el numero es par, o el string impar si el numero es impar*/
 
 const esParOImpar = (numero) => (numero % 2 == 0) ? 'par' : 'impar';
@@ -116,7 +168,109 @@ console.log(esParOImpar(98)); // 'par'
 console.log(esParOImpar(55)); // 'impar'
 console.log(esParOImpar(1042)); // 'par'*/
 
-/* 6) Crear un programa que muestre el dinero inicial, y que permita retirar dinero preguntando 
+/* 10) Crear una función esPositivoONegativo que acepte como argumento un numero y devuelva 
+el string positivo si el numero es positivo, o el string negativo si el numero es negativo*/
+
+
+const esPositivoONegativo = (num) => {
+    if (num >= 0) {
+        return "positivo"
+    }
+    else {
+        return "negativo"
+    }
+}
+
+/*console.log(esPositivoONegativo(3))  // 'positivo'
+console.log(esPositivoONegativo(-5)) // 'negativo'*/
+
+  
+/* 11) Crear una función avanzarSemaforo que acepte como argumento un string colorActual 
+y devuelva un string con el siguiente color del semáforo, siguiendo el 
+orden: verde -> amarillo -> rojo -> verde*/
+
+const avanzarSemaforo = (colorActual) => {
+    switch (colorActual) {
+        case "verde" : return "amarillo";
+        break;
+        case "amarillo" : return "rojo";
+        break;
+        case "rojo" : return "verde";
+    }
+}
+
+/*console.log(avanzarSemaforo('verde'))     // 'amarillo'
+console.log(avanzarSemaforo('amarillo'))  // 'rojo'
+console.log(avanzarSemaforo('rojo'))      // 'verde'*/
+
+
+/* 12) Crear una función obtenerDiasMes que tome como argumento un string mes y devuelva 
+un número dependiendo de la cantidad de días que tenga ese mes*/
+
+const obtenerDiasMes = (mes) => {
+    const mesesLargos= ["enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"];
+    const mesesCortos= ["abril", "junio", "septiembre", "noviembre"];
+    if (mesesLargos.includes(mes)) {
+        return "31";
+    }
+    if (mesesCortos.includes(mes)) {
+        return "30";
+    }
+    if (mes == "febrero"){
+        return "29";
+    }
+}
+
+console.log(obtenerDiasMes("diciembre")) // 31
+console.log(obtenerDiasMes("febrero"))   // 29
+
+
+
+/* 13) Crear una función obtenerGeneracion que tome como argumento un número anioNacimiento 
+y devuelva un string con la generación a la que pertenece, siguientdo estas reglas:
+Baby boomer	   => 1949-1968
+Generación X   => 969-1980
+Millennials	   => 1981-1993
+Generación Z   => 1994-2010*/
+
+
+
+
+/* 14) Crear una función obtenerSensacion que tome como argumento un número temperatura y 
+devuelva un string dependiendo de la temperatura, con las siguientes reglas:
+Menor a 0°	                                => ¡Está helando!
+Mayor o igual a 0° y menor a 15°	        => ¡Hace frío!
+Mayor o igual a 15° y menor a 25°	        => Está lindo
+Mayor o igual a entre 25° y menor a 30°	    => Hace calor
+Mayor o igual de 30°	                    => ¡Hace mucho calor!*/
+
+
+
+/* 15)Crear una función obtenerNota que tome como argumento un número puntaje y devuelva un 
+string dependiendo del puntaje redondeado, con las siguientes reglas:*/
+// Menor a 6	                        => Desaprobado
+// Mayor o igual a 6 y menor a 7	    => Regular
+// Mayor o igual a 7 y menor a 8	    => Bueno
+// Mayor o igual a entre 8 y menor a 10 => Muy bueno
+// 10	                                => Excelente
+// Menor a 0 o mayor a 10	            => Puntaje inválido
+
+
+/* 16) Crear una función jugarPiedraPapelTijera que tome como argumentos dos strings a y b 
+que representen una jugada (piedra, papel, tijera) y dependiendo el devuelva un string con 
+un mensaje avisando qué jugada ganó (o si hubo empate)*/
+
+/*jugarPiedraPapelTijera('tijera', 'piedra')  // ¡Ganó piedra!
+jugarPiedraPapelTijera('piedra', 'tijera')  // ¡Ganó piedra!
+jugarPiedraPapelTijera('papel', 'piedra')   // ¡Ganó papel!
+jugarPiedraPapelTijera('piedra', 'papel')   // ¡Ganó papel!
+jugarPiedraPapelTijera('papel', 'tijera')   // ¡Ganó tijera!
+jugarPiedraPapelTijera('tijera', 'papel')   // ¡Ganó tijera!
+jugarPiedraPapelTijera('piedra', 'piedra')  // ¡Empate!
+jugarPiedraPapelTijera('papel', 'papel')    // ¡Empate!
+jugarPiedraPapelTijera('tijera', 'tijera')  // ¡Empate!*/
+
+/* 17) Crear un programa que muestre el dinero inicial, y que permita retirar dinero preguntando 
 cuánto se desea retirar, y mostrando el dinero restante a continuación. Si se intenta retirar más 
 dinero del disponible, o no cuenta con dinero disponible, debe mostrar un mensaje alertándolo e 
 impedir seguir retirando.*/
@@ -133,7 +287,8 @@ const retirarDinero = (monto) => {
 }
 //console.log(retirarDinero(40000));
 
-/* 7) Crear un programa que simule un Tamagochi (mascota virtual). Debe tener 4 variables: salud, 
+
+/* B01) Crear un programa que simule un Tamagochi (mascota virtual). Debe tener 4 variables: salud, 
 felicidad, limpieza, energía, cuyos valores pueden ir entre 1 y 10. El programa debe iniciar mostrando 
 un mensaje con el nombre del tamagochi y los valores de cada variable (que son asignados aleatoriamente). 
 Luego, debe mostrar las siguientes posibles acciones a realizar, que modifican los valores de la siguiente forma:
@@ -208,19 +363,6 @@ const tamagochi = (accion) => {
 
 //console.log(tamagochi("acariciar"))
 
-/* 8) Crear una función esHoraValida que tome como argumento un string hora con el formato HH:mm y 
-determine si es una hora válida del día o no*/
 
-const esHoraValida = (str) => {
-    const hora = str.split(":")
-    //console.log(hora)
-    return (hora[0] <= 23 && hora[1] <= 59)
-}
-
-
-console.log (esHoraValida('12:23')) // true
-console.log (esHoraValida('12:65')) // false
-console.log (esHoraValida('28:05')) // false
-console.log (esHoraValida('00:00')) // true
 
 
