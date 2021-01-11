18df57e4c48286fd615063d0985af5aeb4e5ff92
Sofia Ferrer <chopotolina@gamil.com>
2021-01-11 15:08:56 -0300

    intro react

---
+++ clase90-REACT-/clase90/src/App.js
@@ -0,0 +1,12 @@
+
+const App = () => {
+  let cont = 4;
+  return (
+    <div className="mi app" id="mi app">
+      <h1>Mi primera app REACT</h1>
+      Mi contador esta en {cont}
+    </div>
+  );
+}
+
+export default App;
